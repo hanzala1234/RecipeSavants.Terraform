54e7c68692c44411d3f1731a26c55bb4f75fc1e3
robert <robert@recipesavants.com>
2019-07-22 04:48:34 -0500

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# RecipeSavants.Terraform
+
+### Showcase
+- Azure
+- RBAC
+- Aks
+- Terraform
+- Helm
+- Lets Encrypt
+
+
+# Clone example repository
+git clone url.git
+cd terraform
+
+# Login to Azure and set subscription
+az login
+
+# Deploy infrastructure
+
+- Initialize terraform
+<pre>
+terraform init
+</pre>
+
+- Check Plan before continuing
+<pre>
+terraform plan
+</pre>
+
+- Appy this command to apply all infrastructure changes
+<pre>
+terraform apply
+</pre>
---
+++ Terraform/modules/environment/variables.tf
@@ -0,0 +1,24 @@
+resource "random_pet" "environment" {
+}
+
+variable "environment" {
+  type        = "string"
+  description = "Name of environment. Used to tag resources."
+  default     = ""
+}
+
+variable "subnet" {
+  type        = "string"
+  description = "Subnet of environment. Used to partition out the address space. Use a /16 space."
+  default     = "15.0.0.0/16"
+}
+
+variable "nodeCount"{
+  type= "string"
+  description = "Domain used for registering SSL certificate on Acme"
+  default = "0"
+}
+
+locals {
+  environment = "${var.environment == "" ? random_pet.environment.id : var.environment}"
+}
